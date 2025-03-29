5ff5582f54fb6639a46cb41a3f9ad5e923efcdc0
joseph chukwuka precious <josephchukwuka4@gmail.com>
2025-03-29 21:27:04 +0100

    update checkEmailExist

--- routes/checkEmailExist.js
+++ routes/checkEmailExist.js
@@ -7,7 +7,7 @@ const { findUserByEmail } = require("../utils/users");
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.post("/", async (req, res) => {
   const { email } = req.body;
   if (!email) {
     return res.status(400).json({ error: true, message: "Provide an email" });
